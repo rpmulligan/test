8d1e9671ad4b08c9d4c37b46626226611bd89056
rpmulligan <rpmulligan@gmail.com>
2015-06-14 18:14:04 +0200

    changed serer.s

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var request = require('request');
 var Lame = require('lame');
 var Speaker = require('speaker');
 
-var text = 'Say hello to my little friend';
+var text = 'You have entered a secure zone please remove yourself in thirty seconds otherwise AK47 shots will be fired in all directions';
 
 var url = 'http://translate.google.com/translate_tts?tl=en&q=' + encodeURIComponent(text);
 request(url).pipe(new Lame.Decoder).pipe(new Speaker);
